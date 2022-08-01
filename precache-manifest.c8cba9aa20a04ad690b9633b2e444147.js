d060a98ca89a2b920436fc9d32a6545534f0a2b7
shehroz22 <94064174+shehroz22@users.noreply.github.com>
2022-08-01 13:25:51 +0800

    Updates

---
+++ precache-manifest.c8cba9aa20a04ad690b9633b2e444147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10af8ee327aa9583fab1",
+    "url": "/react-deploy/static/css/main.4fec8387.chunk.css"
+  },
+  {
+    "revision": "10af8ee327aa9583fab1",
+    "url": "/react-deploy/static/js/main.10af8ee3.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/react-deploy/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/react-deploy/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "2beef90edda9cd71d1a3",
+    "url": "/react-deploy/static/js/runtime~main.2beef90e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-deploy/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "aa431f2d89c620364bbe07982906cf17",
+    "url": "/react-deploy/index.html"
+  }
+];
